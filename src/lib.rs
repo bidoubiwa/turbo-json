010725e9d0d9a9de27028dadcfe4b9a5b344a254
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-09-19 22:04:37 +0200

    Add progress bar

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,63 @@
 use indicatif::ParallelProgressIterator;
-use indicatif::ProgressIterator;
+use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
+use std::fmt::Display;
 use std::io::prelude::*;
 use std::io::{self, ErrorKind};
 use std::{fs::File, io::Cursor};
-use turbo_json_checker::JsonType;
-use std::fmt::Display;
 use termion::{color, style};
-
+use turbo_json_checker::JsonType;
 
 enum JSONFileError {
     IOError(std::io::Error, String),
-    InvalidJSON(std::io::Error, String)
+    InvalidJSON(std::io::Error, String),
 }
 
 impl Display for JSONFileError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             JSONFileError::IOError(error, file_path) => {
-                write!(f, "{}[Input/Output Error]{} {} on {} {} {}", color::Fg(color::Red), style::Reset, error, color::Fg(color::Red), file_path, style::Reset)
+                write!(
+                    f,
+                    "{}[Input/Output Error]{} {}. Could not read/write: {} {} {}",
+                    color::Fg(color::Red),
+                    style::Reset,
+                    error,
+                    color::Fg(color::Red),
+                    file_path,
+                    style::Reset
+                )
             }
             JSONFileError::InvalidJSON(error, file_path) => {
-                write!(f, "{}[Invalid JSON Error]{} {} on {} {} {}", color::Fg(color::Red), style::Reset, error, color::Fg(color::Cyan), file_path, style::Reset)
+                write!(
+                    f,
+                    "{}[Invalid JSON Error]{} {}. Could not read/write: {} {} {}",
+                    color::Fg(color::Red),
+                    style::Reset,
+                    error,
+                    color::Fg(color::Cyan),
+                    file_path,
+                    style::Reset
+                )
             }
         }
     }
 }
 
-
-fn trim_reader_to_content(mut reader: impl Read + Seek, start: u64, end: u64) -> io::Result<impl Read> {
+fn trim_reader_to_content(
+    mut reader: impl Read + Seek,
+    start: u64,
+    end: u64,
+) -> io::Result<impl Read> {
     reader.seek(io::SeekFrom::Start(start))?;
     Ok(reader.take(end - start))
 }
 
-fn flatten_reader_on_array(reader: impl Read + Seek, start: u64, end: u64) -> io::Result<Option<impl Read>> {
+fn flatten_reader_on_array(
+    reader: impl Read + Seek,
+    start: u64,
+    end: u64,
+) -> io::Result<Option<impl Read>> {
     let mut trimmed_reader = trim_reader_to_content(reader, start, end)?;
     let mut c = [0];
 
@@ -61,13 +85,20 @@ fn flatten_reader_on_array(reader: impl Read + Seek, start: u64, end: u64) -> io
     Ok(Some(Cursor::new(c.clone()).chain(trimmed_reader)))
 }
 
-
 fn validate_files(
     files_path: &[String],
 ) -> Vec<Result<Option<Box<dyn Read + Send>>, JSONFileError>> {
+    let number_of_files = files_path.len() as u64;
+    let files_validator_progress_bar = ProgressBar::new(number_of_files);
+    files_validator_progress_bar.set_style(
+        ProgressStyle::default_bar()
+            .template("{spinner:.green} [{elapsed_precise}] [{bar:.green}]▏{pos}/{len}")
+            .progress_chars("█▇▆▅▄▃▂▁  "),
+    );
+
     files_path
         .par_iter()
-        .progress_count(files_path.len() as u64)
+        .progress_with(files_validator_progress_bar)
         .map(|file_path| {
             let file = match File::open(file_path) {
                 Ok(file) => file,
@@ -81,18 +112,16 @@ fn validate_files(
                         Ok(cursor_option) => Ok(
                             cursor_option.map(|reader| Box::new(reader) as Box<dyn Read + Send>)
                         ),
-                        Err(error) => {
-                            Err(JSONFileError::IOError(error, file_path.to_string())
-                        )},
+                        Err(error) => Err(JSONFileError::IOError(error, file_path.to_string())),
                     }
                 }
-                Ok((_, start, end)) => match trim_reader_to_content(file, start as u64, end as u64 + 1) {
-                    Ok(cursor) => Ok(Some(Box::new(cursor) as Box<dyn Read + Send>)),
-                    Err(error) => Err(JSONFileError::IOError(error, file_path.to_string()))
-                },
-                Err(error) => {
-                    Err(JSONFileError::InvalidJSON(error, file_path.to_string()))
+                Ok((_, start, end)) => {
+                    match trim_reader_to_content(file, start as u64, end as u64 + 1) {
+                        Ok(cursor) => Ok(Some(Box::new(cursor) as Box<dyn Read + Send>)),
+                        Err(error) => Err(JSONFileError::IOError(error, file_path.to_string())),
+                    }
                 }
+                Err(error) => Err(JSONFileError::InvalidJSON(error, file_path.to_string())),
             }
         })
         .collect()
@@ -103,20 +132,24 @@ pub fn json_combine(file_paths: Vec<String>, mut writer: impl Write) {
         eprintln!("{}", error);
         panic!("Could not write in the output stream");
     }
-    let mut error_messages = Vec::new();
     let mut first_element = true;
 
+    // Create progress bar
     let valid_files_reader = validate_files(&file_paths);
     let number_of_files = valid_files_reader.len() as u64;
-    for valid_files_reader in valid_files_reader
-        .into_iter()
-        .progress_count(number_of_files)
-    {
+    let files_write_progress_bar = ProgressBar::new(number_of_files);
+    files_write_progress_bar.set_style(
+        ProgressStyle::default_bar()
+            .template("{spinner:.green} [{elapsed_precise}] [{bar:.green}]▏{pos}/{len}") // green does not work, it is always white
+            .progress_chars("█▇▆▅▄▃▂▁  "),
+    );
+
+    for valid_files_reader in valid_files_reader.into_iter() {
         let mut file_reader = match valid_files_reader {
             Ok(Some(reader)) => reader,
             Ok(None) => continue,
             Err(error) => {
-                error_messages.push(error);
+                files_write_progress_bar.println(error.to_string());
                 continue;
             }
         };
@@ -133,11 +166,13 @@ pub fn json_combine(file_paths: Vec<String>, mut writer: impl Write) {
         if let Err(error) = io::copy(&mut file_reader, &mut writer) {
             panic!("{}", error);
         }
+
+        // Increment progress bar by one
+        files_write_progress_bar.inc(1);
     }
 
     if let Err(error) = writer.write_all(b"]") {
         eprintln!("{}", error);
         panic!("Could not write in the output stream");
     }
-    error_messages.iter().for_each(|error| eprintln!("{}", error))
 }
